5982616de8126cb8edd9966737bda46d5f59a155
Ahmad Habib <itheahmadx.com>
2021-08-30 11:33:45 +0500

    Game Stats: Dice roll complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,18 @@
 let diceBtn = document.querySelector(".roll-btn");
 let diceNumber = document.querySelector(".dice-number");
+let dice = document.querySelector(".dice");
+
+diceBtn.addEventListener("click", rollDice);
+
+/* DICE ROLL */
+function rollDice() {
+    dice.classList.add("spin");
+
+    let roll = Math.floor(Math.random() * 6) + 1;
+    setTimeout(()=>{
+        dice.classList.remove("spin");
+        diceNumber.innerText = roll;
+    }, 2000);
+
+    diceBtn.removeEventListener('click', () => { });
+}
