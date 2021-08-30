e097a67b1e0adaa098a459b0256cb32d206c89d1
Ahmad Habib <itheahmadx.com>
2021-08-30 11:50:42 +0500

    Game Stats: Ask number of players setup complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,27 @@
 let diceBtn = document.querySelector(".roll-btn");
 let diceNumber = document.querySelector(".dice-number");
 let dice = document.querySelector(".dice");
+let roll = 0;
+let numUsers;
 
+window.onload = () => {
+    let numUsers = prompt("How many plaers are playing? (2 - 4)", "2");
+    if(parseInt(numUsers) < 2 || parseInt(numUsers) > 4) {
+        window.location.reload();
+    }
+    numUsers = parseInt(numUsers);
+};
+
+/* EVENT HANDLER */
 diceBtn.addEventListener("click", rollDice);
 
 /* DICE ROLL */
 function rollDice() {
+    diceNumber.innerText = "";
     dice.classList.add("spin");
 
-    let roll = Math.floor(Math.random() * 6) + 1;
-    setTimeout(()=>{
+    roll = Math.floor(Math.random() * 6) + 1;
+    setTimeout(() => {
         dice.classList.remove("spin");
         diceNumber.innerText = roll;
     }, 2000);
